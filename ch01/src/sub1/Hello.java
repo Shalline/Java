7d12ec2e7d39dc32e8bf0b86b5f0c4f3a6ac9be5
machinemayou <machinemayou@gmail.com>
2025-07-10 16:45:42 +0900

    add ch01

---
+++ ch01/src/sub1/Hello.java
@@ -0,0 +1,18 @@
+package sub1;
+
+/*
+	날짜 : 2025/07/07
+	이름 : 김수진
+	내용 : 1장 JAVA 개발환경 구축 실습
+ */
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
+		
+	}
+	
+}
