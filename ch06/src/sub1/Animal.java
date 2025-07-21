171147b0640db8bcddc85a12d6cb58c38c786eeb
machinemayou <machinemayou@gmail.com>
2025-07-21 12:41:52 +0900

    add ch06 am upload

---
+++ ch06/src/sub1/Animal.java
@@ -0,0 +1,10 @@
+package sub1;
+
+public class Animal {
+	
+	public void move() {
+		System.out.println("Animal move...");
+	}
+	// 오버라이드를 위한 매서드이기 때문에 매서드 내용은 필요 없음
+	public void hunt() {}
+}
---
+++ ch06/src/sub1/Egle.java
@@ -0,0 +1,14 @@
+package sub1;
+
+public class Egle extends Animal{
+
+	@Override
+	public void move() {
+		
+		System.out.println("Egle fly...");
+	}
+	@Override
+	public void hunt() {
+		System.out.println("Egle hunt...");
+	}
+}
